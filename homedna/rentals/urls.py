b53a06f06f8ae18da992cd09188e598933d65682
Renze Wang <mikeyrxwin@gmail.com>
2023-06-02 08:33:06 +0000

    init commit

---
+++ homedna/rentals/urls.py
@@ -0,0 +1,14 @@
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    # ex： /rentals/
+    path("", views.index, name="index"),
+    # ex: /rentals/5/
+    path("<int:question_id>/", views.detail, name="detail"),
+    # ex: /rentals/5/results/
+    path("<int:question_id>/results/", views.results, name="results"),
+    # ex: /rentals/5/vote/
+    path("<int:question_id>/vote/", views.vote, name="vote"),
+]
