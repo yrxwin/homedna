8690d7a209908df0a50ae4c91d0623191b63d110
Renze Wang <mikeyrxwin@gmail.com>
2023-11-12 09:41:56 +0000

    follow tutorial part 1 - 4

--- homedna/rentals/urls.py
+++ homedna/rentals/urls.py
@@ -4,12 +4,8 @@ from . import views
 
 app_name = "rentals"
 urlpatterns = [
-    # ex： /rentals/
-    path("", views.index, name="index"),
-    # ex: /rentals/5/
-    path("<int:question_id>/", views.detail, name="detail"),
-    # ex: /rentals/5/results/
-    path("<int:question_id>/results/", views.results, name="results"),
-    # ex: /rentals/5/vote/
+    path("", views.IndexView.as_view(), name="index"),
+    path("<int:pk>/", views.DetailView.as_view(), name="detail"),
+    path("<int:pk>/results/", views.ResultsView.as_view(), name="results"),
     path("<int:question_id>/vote/", views.vote, name="vote"),
 ]
